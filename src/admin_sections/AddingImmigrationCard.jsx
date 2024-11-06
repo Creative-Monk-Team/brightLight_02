d1eae5674316bb6661be712ea29d34598cdedfa2
UttamCreativeMonk <uttam.creativemonk@gmail.com>
2024-11-06 15:45:15 +0530

    selectionTagDataAdded

--- src/admin_sections/AddingImmigrationCard.jsx
+++ src/admin_sections/AddingImmigrationCard.jsx
@@ -145,10 +145,7 @@ let AddImmigrationToolsCard = () => {
         disabled={!editMode}
       >
         <option value="#"># (For Non Route / Coming Soon)</option>
-        <option value="booking">
-          Free Assesement
-          (/booking)
-        </option>
+        <option value="booking">Free Assesement (https://api.leadconnectorhq.com/widget/booking/Tg8EPG2CVEMkQ1J0F3yj)</option>
         <option value="federal-skilled">FSWP Calc</option>
         <option value="bcpnp-calculator">BCPNP Calc</option>
         <option value="clb-ilets-calculator?selected=type1">
@@ -161,9 +158,139 @@ let AddImmigrationToolsCard = () => {
           CLB Calc for TEF (French)
         </option>
         <option value="category-based">Category Based Draws</option>
-        <option value="link8">link 8</option>
-        <option value="link9">link 9</option>
-        <option value="link10">link 10</option>
+        <option value="skilled-worker-stream">Skilled Worker Stream</option>
+        <option value="about-us">About Us</option>
+        <option value="bcpnp-calculator">Bcpnp Calculator</option>
+        <option value="federal-skilled">Federal Skilled</option>
+        <option value="contact-us">Contact Us</option>
+        <option value="express-entry">Express Entry</option>
+        <option value="permanent-residency">Permanent Residency</option>
+        <option value="transport-occupation">Transport Occupation</option>
+        <option value="priorities-program">Priorities Program</option>
+        <option value="international-post-graduate-program">
+          International Post Graduate Program
+        </option>
+        <option value="international-graduate-program">
+          International Graduate Program
+        </option>
+        <option value="health-authority-stream">Health Authority Stream</option>
+        <option value="healthcare-targeted-draw">
+          Healthcare Targeted Draw
+        </option>
+        <option value="entry-level-semi-skilled">
+          Entry Level Semi Skilled
+        </option>
+        <option value="bc-pnp">Bc Pnp</option>
+        <option value="pnp">Pnp</option>
+        <option value="rnip">Rnip</option>
+        <option value="agri-food-pilot-program">Agri Food Pilot Program</option>
+        <option value="pilot-programs">Pilot Programs</option>
+        <option value="transport-occupation-targeted-draw">
+          Transport Occupation Targeted Draw
+        </option>
+        <option value="trade-occupation-targeted-draw">
+          Trade Occupation Targeted Draw
+        </option>
+        <option value="stem-targeted-draw">Stem Targeted Draw</option>
+        <option value="french-targeted-draw">French Targeted Draw</option>
+        <option value="federal-skilled-worker-program">
+          Federal Skilled Worker Program
+        </option>
+        <option value="federal-skilled-trades-program">
+          Federal Skilled Trades Program
+        </option>
+        <option value="canadian-experience-class">
+          Canadian Experience Class
+        </option>
+        <option value="dual-intent-visa">Dual Intent Visa</option>
+        <option value="business-visitor-visa">Business Visitor Visa</option>
+        <option value="visitor-visa">Visitor Visa</option>
+        <option value="temporary-resident">Temporary Resident</option>
+        <option value="temporary-resident-permit-draft">
+          Temporary Resident Permit Draft
+        </option>
+        <option value="super-visa">Super Visa</option>
+        <option value="cby">Cby</option>
+        <option value="common-law-partner-temporary">
+          Common Law Partner Temporary
+        </option>
+        <option value="common-law-partner-permanent">
+          Common Law Partner Permanent
+        </option>
+        <option value="common-law-partner-international">
+          Common Law Partner International
+        </option>
+        <option value="spouse-common-law-sponsership">
+          Spouse Common Law Sponsorship
+        </option>
+        <option value="restoration-status-draft">
+          Restoration Status Draft
+        </option>
+        <option value="spousal-open-work-permit">
+          Spousal Open Work Permit
+        </option>
+        <option value="open-work-permit-for-spouse-inland">
+          Open Work Permit for Spouse Inland
+        </option>
+        <option value="flagpoling">Flagpoling</option>
+        <option value="extensions-draft">Extensions Draft</option>
+        <option value="study-permit-minors">Study Permit Minors</option>
+        <option value="sds">Sds</option>
+        <option value="non-sds">Non Sds</option>
+        <option value="outside-canada">Outside Canada</option>
+        <option value="visitor-to-student">Visitor to Student</option>
+        <option value="change-college-program">Change College Program</option>
+        <option value="inside-canada">Inside Canada</option>
+        <option value="student-visa">Student Visa</option>
+        <option value="parentsgrandparents">Parents Grandparents</option>
+        <option value="orphan">Orphan</option>
+        <option value="lonely-canadian">Lonely Canadian</option>
+        <option value="humanitarian-compassionate">
+          Humanitarian Compassionate
+        </option>
+        <option value="dependent-children">Dependent Children</option>
+        <option value="adoption">Adoption</option>
+        <option value="family-reunification-sponsorship">
+          Family Reunification Sponsorship
+        </option>
+        <option value="pgwp">Pgwp</option>
+        <option value="openWork-dependent-children">
+          Open Work Dependent Children
+        </option>
+        <option value="open-work-vulnerable-lp">Open Work Vulnerable LP</option>
+        <option value="francophone-mobility-program">
+          Francophone Mobility Program
+        </option>
+        <option value="bridging-open-work-permit-lp">
+          Bridging Open Work Permit LP
+        </option>
+        <option value="low-wage-lmia">Low Wage Lmia</option>
+        <option value="global-stream-lmia">Global Stream Lmia</option>
+        <option value="agriculture-stream-lmia">Agriculture Stream Lmia</option>
+        <option value="open-work-permit">Open Work Permit</option>
+        <option value="lmia-reviewed">Lmia Reviewed</option>
+        <option value="reply-to-pfl-page">Reply to PFL Page</option>
+        <option value="additional-document">Additional Document</option>
+        <option value="reconsideration">Reconsideration</option>
+        <option value="citizenship">Citizenship</option>
+        <option value="pr-renewal">Pr Renewal</option>
+        <option value="work-permit">Work Permit</option>
+        <option value="category-based-express">Category Based Express</option>
+        <option value="agriculture-agri-food-occupation">
+          Agriculture Agri Food Occupation
+        </option>
+        <option value="in-home-caregiver-program-lp">
+          In Home Caregiver Program LP
+        </option>
+        <option value="pathways-for-caregiver">Pathways for Caregiver</option>
+        <option value="permanent-residence-pathways-caregivers-lp">
+          Permanent Residence Pathways Caregivers LP
+        </option>
+        <option value="booking">Booking</option>
+        <option value="spouse-inland">Spouse Inland</option>
+        <option value="spouse-outland">Spouse Outland</option>
+        <option value="same-sex">Same Sex</option>
+        <option value="immigration-tools">Immigration Tools</option>
       </select>
       <p className={styles.shortLabel}>Blue Image Below</p>
       <input
@@ -182,7 +309,7 @@ let AddImmigrationToolsCard = () => {
       />
       {sectionDataSingle.blue_stroke_img && (
         <img
-          className={styles.existingImageSmall}
+          className={styles.existingImageSmall2}
           src={sectionDataSingle.blue_stroke_img}
           alt="Preview"
         />
@@ -190,12 +317,11 @@ let AddImmigrationToolsCard = () => {
 
       {sectionDataSingle.white_stroke_img && (
         <img
-          className={styles.existingImageSmall}
+          className={`${styles.existingImageSmall2} ${styles.blueImage}`}
           src={sectionDataSingle.white_stroke_img}
           alt="Preview"
         />
       )}
-
       <div className={styles.editIcons}>
         {editMode ? (
           <img
