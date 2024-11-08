1b0de31485c948a284478921e53e095b523a7eb7
Creative-Monk-Team <team.creativemonk@gmail.com>
2024-11-08 10:30:31 +0530

    adding faq's and testimonials in category based draws panel page

--- src/admin_sections/Category.jsx
+++ src/admin_sections/Category.jsx
@@ -69,6 +69,30 @@ const CategoryBased = () => {
     wcu2: "",
     wcu3: "",
     wcu4: "",
+
+    faq_heading: "",
+    q1: "",
+    qa1: "",
+    q2: "",
+    qa2: "",
+    q3: "",
+    qa3: "",
+    q4: "",
+    qa4: "",
+    q5: "",
+    qa5: "",
+    q6: "",
+    qa6: "",
+    q7: "",
+    qa7: "",
+    q8: "",
+    qa8: "",
+    q9: "",
+    qa9: "",
+    q10: "",
+    qa10: "",
+
+    show_testimonials: "",
   });
 
   const [editMode, setEditMode] = useState(false);
@@ -474,6 +498,179 @@ const CategoryBased = () => {
         onChange={handleInputChange}
         disabled={!editMode}
       />
+
+      <h1 className={styles.faqStartsHeading}>FAQ's Starts Below</h1>
+
+      <input
+        placeholder="FAQ's Made Simple"
+        name="faq_heading"
+        value={sectionData.faq_heading || ""}
+        onChange={handleInputChange}
+        disabled={!editMode}
+      />
+      <input
+        placeholder="Question 1"
+        name="q1"
+        value={sectionData.q1 || ""}
+        onChange={handleInputChange}
+        disabled={!editMode}
+      />
+      <textarea
+        placeholder="Answer 1"
+        name="qa1"
+        value={sectionData.qa1 || ""}
+        onChange={handleInputChange}
+        disabled={!editMode}
+      />
+      <input
+        placeholder="Question 2"
+        name="q2"
+        value={sectionData.q2 || ""}
+        onChange={handleInputChange}
+        disabled={!editMode}
+      />
+      <textarea
+        placeholder="Answer 2"
+        name="qa2"
+        value={sectionData.qa2 || ""}
+        onChange={handleInputChange}
+        disabled={!editMode}
+      />
+      <input
+        placeholder="Question 3"
+        name="q3"
+        value={sectionData.q3 || ""}
+        onChange={handleInputChange}
+        disabled={!editMode}
+      />
+      <textarea
+        placeholder="Answer 3"
+        name="qa3"
+        value={sectionData.qa3 || ""}
+        onChange={handleInputChange}
+        disabled={!editMode}
+      />
+      <input
+        placeholder="Question 4"
+        name="q4"
+        value={sectionData.q4 || ""}
+        onChange={handleInputChange}
+        disabled={!editMode}
+      />
+      <textarea
+        placeholder="Answer 4"
+        name="qa4"
+        value={sectionData.qa4 || ""}
+        onChange={handleInputChange}
+        disabled={!editMode}
+      />
+      <input
+        placeholder="Question 5"
+        name="q5"
+        value={sectionData.q5 || ""}
+        onChange={handleInputChange}
+        disabled={!editMode}
+      />
+      <textarea
+        placeholder="Answer 5"
+        name="qa5"
+        value={sectionData.qa5 || ""}
+        onChange={handleInputChange}
+        disabled={!editMode}
+      />
+      <input
+        placeholder="Question 6"
+        name="q6"
+        value={sectionData.q6 || ""}
+        onChange={handleInputChange}
+        disabled={!editMode}
+      />
+      <textarea
+        placeholder="Answer 6"
+        name="qa6"
+        value={sectionData.qa6 || ""}
+        onChange={handleInputChange}
+        disabled={!editMode}
+      />
+      <input
+        placeholder="Question 7"
+        name="q7"
+        value={sectionData.q7 || ""}
+        onChange={handleInputChange}
+        disabled={!editMode}
+      />
+      <textarea
+        placeholder="Answer 7"
+        name="qa7"
+        value={sectionData.qa7 || ""}
+        onChange={handleInputChange}
+        disabled={!editMode}
+      />
+      <input
+        placeholder="Question 8"
+        name="q8"
+        value={sectionData.q8 || ""}
+        onChange={handleInputChange}
+        disabled={!editMode}
+      />
+      <textarea
+        placeholder="Answer 8"
+        name="qa8"
+        value={sectionData.qa8 || ""}
+        onChange={handleInputChange}
+        disabled={!editMode}
+      />
+      <input
+        placeholder="Question 9"
+        name="q9"
+        value={sectionData.q9 || ""}
+        onChange={handleInputChange}
+        disabled={!editMode}
+      />
+      <textarea
+        placeholder="Answer 9"
+        name="qa9"
+        value={sectionData.qa9 || ""}
+        onChange={handleInputChange}
+        disabled={!editMode}
+      />
+      <input
+        placeholder="Question 10"
+        name="q10"
+        value={sectionData.q10 || ""}
+        onChange={handleInputChange}
+        disabled={!editMode}
+      />
+      <textarea
+        placeholder="Answer 10"
+        name="qa10"
+        value={sectionData.qa10 || ""}
+        onChange={handleInputChange}
+        disabled={!editMode}
+      />
+
+      <h1 className={styles.faqEndHeading}>FAQ's Ends here</h1>
+
+      <h1 className={styles.faqStartsHeading}>
+        Testimonials Visibility Control
+      </h1>
+
+      <div className={styles.testimonialsVisibility}>
+        <p>Want to display Testimonials Section</p>
+        <input
+          placeholder="Show Testimonials"
+          name="show_testimonials"
+          value={sectionData.show_testimonials || ""}
+          onChange={handleInputChange}
+          disabled={!editMode}
+        />
+      </div>
+      <p className={styles.testimonialsDisclamier}>
+        Note: Testimonials Visibility On the Selected Page is totally dependent
+        on the input value above. If you want to display the section , just
+        write "Y" without quotes , anything else will be considered as "N" even
+        "y". If not want to display then just write "N" without qoutes.
+      </p>
 
       <div className={styles.editIcons}>
         {editMode ? (
